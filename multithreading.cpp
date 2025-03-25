f5f9d3a8c2c6569212b203fb82c46c26bc4a9706
Sagar More <sagarmore1007@gmail.com>
2025-03-25 23:40:34 +0530

    Multithreading

---
+++ multithreading.cpp
@@ -0,0 +1,36 @@
+#include<iostream> // for input and output
+#include<thread> // for threading
+#include<chrono> // for time functions
+
+using namespace std;  // for standad names
+
+void _thread1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "*";
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+    
+}
+
+void _thread2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "o";
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+    
+}
+
+
+int main()
+{
+    thread worker1(_thread1);
+    thread worker2(_thread2);
+
+    worker1.join();
+    worker2.join();
+    cout<< "\n";
+}
